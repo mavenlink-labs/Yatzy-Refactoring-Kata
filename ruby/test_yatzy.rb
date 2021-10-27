37ecb4ba84143354f12e71a3fd58f1a4c4b11a9a
Brittany Thompson <brittanythomps@gmail.com>
2021-10-27 15:37:29 -0600

    the linter isnt mad anymore so thats something

--- ruby/test_yatzy.rb
+++ ruby/test_yatzy.rb
@@ -3,93 +3,89 @@ require 'test/unit'
 
 class YatzyTest < Test::Unit::TestCase
   def test_chance_scores_sum_of_all_dice
-    expected = 15
-    actual = Yatzy.chance(2,3,4,5,1)
-    assert expected == actual
-    assert 16 == Yatzy.chance(3,3,4,5,1)
+    assert_equal 15, Yatzy.new(2, 3, 4, 5, 1).chance
+    assert_equal 16, Yatzy.new(3, 3, 4, 5, 1).chance
   end
 
   def test_yatzy_scores_50
-    expected = 50
-    actual = Yatzy.yatzy([4,4,4,4,4])
-    assert expected == actual
-    assert 50 == Yatzy.yatzy([6,6,6,6,6])
-    assert 0 == Yatzy.yatzy([6,6,6,6,3])
+    assert_equal 50, Yatzy.new(4, 4, 4, 4, 4).yatzy
+    assert_equal 50, Yatzy.new(6, 6, 6, 6, 6).yatzy
+    assert_equal 0, Yatzy.new(6, 6, 6, 6, 3).yatzy
   end
 
   def test_1s
-    assert Yatzy.new(1, 2, 3, 4, 5).addOfAKind(1) == 1
-    assert 2 == Yatzy.new(1,2,1,4,5).addOfAKind(1)
-    assert 0 == Yatzy.new(6,2,2,4,5).addOfAKind(1)
-    assert 4 == Yatzy.new(1,2,1,1,1).addOfAKind(1)
+    assert_equal 1, Yatzy.new(1, 2, 3, 4, 5).add_of_a_kind(1)
+    assert_equal 2, Yatzy.new(1, 2, 1, 4, 5).add_of_a_kind(1)
+    assert_equal 0, Yatzy.new(6, 2, 2, 4, 5).add_of_a_kind(1)
+    assert_equal 4, Yatzy.new(1, 2, 1, 1, 1).add_of_a_kind(1)
   end
 
   def test_2s
-    assert Yatzy.new(1,2,3,2,6).addOfAKind(2) == 4
-    assert Yatzy.new(2,2,2,2,2).addOfAKind(2) == 10
+    assert_equal Yatzy.new(1, 2, 3, 2, 6).add_of_a_kind(2), 4
+    assert_equal Yatzy.new(2, 2, 2, 2, 2).add_of_a_kind(2), 10
   end
 
   def test_threes
-    assert 6 == Yatzy.new(1,2,3,2,3).addOfAKind(3)
-    assert 12 == Yatzy.new(2,3,3,3,3).addOfAKind(3)
+    assert_equal 6, Yatzy.new(1, 2, 3, 2, 3).add_of_a_kind(3)
+    assert_equal 12, Yatzy.new(2, 3, 3, 3, 3).add_of_a_kind(3)
   end
 
   def test_fours_test
-    assert 12 == Yatzy.new(4,4,4,5,5).addOfAKind(4)
-    assert 8 == Yatzy.new(4,4,5,5,5).addOfAKind(4)
-    assert 4 == Yatzy.new(4,5,5,5,5).addOfAKind(4)
+    assert_equal 12, Yatzy.new(4, 4, 4, 5, 5).add_of_a_kind(4)
+    assert_equal 8, Yatzy.new(4, 4, 5, 5, 5).add_of_a_kind(4)
+    assert_equal 4, Yatzy.new(4, 5, 5, 5, 5).add_of_a_kind(4)
   end
 
-  def test_fives()
-    assert 10 == Yatzy.new(4,4,4,5,5).addOfAKind(5)
-    assert 15 == Yatzy.new(4,4,5,5,5).addOfAKind(5)
-    assert 20 == Yatzy.new(4,5,5,5,5).addOfAKind(5)
+  def test_fives
+    assert_equal 10, Yatzy.new(4, 4, 4, 5, 5).add_of_a_kind(5)
+    assert_equal 15, Yatzy.new(4, 4, 5, 5, 5).add_of_a_kind(5)
+    assert_equal 20, Yatzy.new(4, 5, 5, 5, 5).add_of_a_kind(5)
   end
 
   def test_sixes_test
-    assert 0 == Yatzy.new(4,4,4,5,5).addOfAKind(6)
-    assert 6 == Yatzy.new(4,4,6,5,5).addOfAKind(6)
-    assert 18 == Yatzy.new(6,5,6,6,5).addOfAKind(6)
+    assert_equal 0, Yatzy.new(4, 4, 4, 5, 5).add_of_a_kind(6)
+    assert_equal 6, Yatzy.new(4, 4, 6, 5, 5).add_of_a_kind(6)
+    assert_equal 18, Yatzy.new(6, 5, 6, 6, 5).add_of_a_kind(6)
   end
 
   def test_one_pair
-    assert 6 == Yatzy.score_pair(3,4,3,5,6)
-    assert 10 == Yatzy.score_pair(5,3,3,3,5)
-    assert 12 == Yatzy.score_pair(5,3,6,6,5)
+    assert_equal 6, Yatzy.new(3, 4, 3, 5, 6).score_pair
+    assert_equal 10, Yatzy.new(5, 3, 3, 3, 5).score_pair
+    assert_equal 12, Yatzy.new(5, 3, 6, 6, 5).score_pair
   end
 
-  def test_two_Pair
-    assert_equal 16, Yatzy.two_pair(3,3,5,4,5)
-    assert_equal 16, Yatzy.two_pair(3,3,5,5,5)
+  def test_two_pair
+    assert_equal 16, Yatzy.new(3, 3, 5, 4, 5).two_pair
+    assert_equal 16, Yatzy.new(3, 3, 5, 5, 5).two_pair
   end
 
-  def test_three_of_a_kind()
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,4,5)
-    assert 15 == Yatzy.three_of_a_kind(5,3,5,4,5)
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,3,5)
+  def test_three_of_a_kind
+    assert_equal 9, Yatzy.new(3, 3, 3, 4, 5).three_of_a_kind
+    assert_equal 15, Yatzy.new(5, 3, 5, 4, 5).three_of_a_kind
+    assert_equal 9, Yatzy.new(3, 3, 3, 3, 5).three_of_a_kind
+    assert_equal 9, Yatzy.new(3, 3, 3, 3, 3).three_of_a_kind
   end
 
   def test_four_of_a_knd
-    assert 12 == Yatzy.four_of_a_kind(3,3,3,3,5)
-    assert 20 == Yatzy.four_of_a_kind(5,5,5,4,5)
-    assert 9 == Yatzy.three_of_a_kind(3,3,3,3,3)
-    assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
+    assert_equal 12, Yatzy.new(3, 3, 3, 3, 5).four_of_a_kind
+    assert_equal 20, Yatzy.new(5, 5, 5, 4, 5).four_of_a_kind
+    assert_equal 12, Yatzy.new(3, 3, 3, 3, 3).four_of_a_kind
   end
 
-  def test_smallStraight()
-    assert 15 == Yatzy.smallStraight(1,2,3,4,5)
-    assert 15 == Yatzy.smallStraight(2,3,4,5,1)
-    assert 0 == Yatzy.smallStraight(1,2,2,4,5)
+  def test_small_straight
+    assert_equal 15, Yatzy.new(1, 2, 3, 4, 5).small_straight
+    assert_equal 15, Yatzy.new(2, 3, 4, 5, 1).small_straight
+    assert_equal 0, Yatzy.new(1, 2, 2, 4, 5).small_straight
   end
 
-  def test_largeStraight
-    assert 20 == Yatzy.largeStraight(6,2,3,4,5)
-    assert 20 == Yatzy.largeStraight(2,3,4,5,6)
-    assert 0 == Yatzy.largeStraight(1,2,2,4,5)
+  def test_large_straight
+    assert_equal 20, Yatzy.new(6, 2, 3, 4, 5).large_straight
+    assert_equal 20, Yatzy.new(2, 3, 4, 5, 6).large_straight
+    assert_equal 0, Yatzy.new(1, 2, 2, 4, 5).large_straight
   end
 
-  def test_fullHouse()
-    assert 18 == Yatzy.fullHouse(6,2,2,2,6)
-    assert 0 == Yatzy.fullHouse(2,3,4,5,6)
+  def test_full_house
+    assert_equal 18, Yatzy.new(6, 2, 2, 2, 6).full_house
+    assert_equal 0, Yatzy.new(2, 3, 4, 5, 6).full_house
   end
 end
--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -1,167 +1,87 @@
 class Yatzy
-  def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+  def initialize(dice1, dice2, dice3, dice4, dice5)
+    @dice = [dice1, dice2, dice3, dice4, dice5]
+    @tallies = tallies
   end
 
-  def self.yatzy(dice)
-    counts = [0]*(dice.length+1)
-    for die in dice do
-      counts[die-1] += 1
+  def tallies
+    tallies = [0] * 7
+    @dice.each do |die|
+      tallies[die] += 1
     end
-    for i in 0..counts.size do
-      if counts[i] == 5
-        return 50
-      end
-    end
-    return 0
+    tallies
   end
 
-  def addOfAKind(value)
+  def chance
+    sum = 0
+    @dice.each { |die| sum += die }
+    sum
+  end
+
+  def yatzy
+    @tallies.include?(5) ? 50 : 0
+  end
+
+  def add_of_a_kind(value)
     sum = 0
     @dice.each do |die|
       sum += die if die == value
     end
-    return sum
+    sum
   end
 
-  def initialize(d1, d2, d3, d4, d5)
-    @dice = [d1, d2, d3, d4, d5]
-  end
-
-  def self.score_pair( d1,  d2,  d3,  d4,  d5)
-    counts = [0]*6
-    counts[d1-1] += 1
-    counts[d2-1] += 1
-    counts[d3-1] += 1
-    counts[d4-1] += 1
-    counts[d5-1] += 1
-    at = 0
-    (0...6).each do |at|
-      if (counts[6-at-1] >= 2)
-        return (6-at)*2
-      end
+  def score_of_a_kind(number)
+    [6, 5, 4, 3, 2, 1].each do |value|
+      return value * number if @tallies[value] >= number
     end
-    return 0
+    0
   end
 
-  def self.two_pair( d1,  d2,  d3,  d4,  d5)
-    counts = [0]*6
-    counts[d1-1] += 1
-    counts[d2-1] += 1
-    counts[d3-1] += 1
-    counts[d4-1] += 1
-    counts[d5-1] += 1
+  def score_pair
+    score_of_a_kind(2)
+  end
+
+  def three_of_a_kind
+    score_of_a_kind(3)
+  end
+
+  def four_of_a_kind
+    score_of_a_kind(4)
+  end
+
+  def two_pair
     n = 0
     score = 0
-    for i in Array 0..5
-      if (counts[6-i-1] >= 2)
-        n = n+1
-        score += (6-i)
-      end
-    end
-    if (n == 2)
-      return score * 2
-    else
-      return 0
-    end
-  end
-
-  def self.four_of_a_kind( _1,  _2,  d3,  d4,  d5)
-    tallies = [0]*6
-    tallies[_1-1] += 1
-    tallies[_2-1] += 1
-    tallies[d3-1] += 1
-    tallies[d4-1] += 1
-    tallies[d5-1] += 1
-    for i in (0..6)
-      if (tallies[i] >= 4)
-        return (i+1) * 4
-      end
-    end
-    return 0
-  end
-
-  def self.three_of_a_kind( d1,  d2,  d3,  d4,  d5)
-    t = [0]*6
-    t[d1-1] += 1
-    t[d2-1] += 1
-    t[d3-1] += 1
-    t[d4-1] += 1
-    t[d5-1] += 1
-    for i in [0,1,2,3,4,5]
-      if (t[i] >= 3)
-        return (i+1) * 3
+    [5, 4, 3, 2, 1].each do |value|
+      if @tallies[value] >= 2
+        score += value
+        n += 1
+        return score * 2 if n == 2
       end
     end
     0
   end
 
-  def self.smallStraight( d1,  d2,  d3,  d4,  d5)
-    tallies = [0]*6
-    tallies[d1-1] += 1
-    tallies[d2-1] += 1
-    tallies[d3-1] += 1
-    tallies[d4-1] += 1
-    tallies[d5-1] += 1
-    (tallies[0] == 1 and
-      tallies[1] == 1 and
-      tallies[2] == 1 and
-      tallies[3] == 1 and
-      tallies[4] == 1) ? 15 : 0
+  def small_straight
+    (1..5).each do |value|
+      return 0 if tallies[value] != 1
+    end
+    15
   end
 
-  def self.largeStraight( d1,  d2,  d3,  d4,  d5)
-    tallies = [0]*6
-    tallies[d1-1] += 1
-    tallies[d2-1] += 1
-    tallies[d3-1] += 1
-    tallies[d4-1] += 1
-    tallies[d5-1] += 1
-    if (tallies[1] == 1 and tallies[2] == 1 and tallies[3] == 1 and tallies[4] == 1 and tallies[5] == 1)
-      return 20
+  def large_straight
+    (2..6).each do |value|
+      return 0 if tallies[value] != 1
     end
-    return 0
+    20
   end
 
-  def self.fullHouse( d1,  d2,  d3,  d4,  d5)
-    tallies = []
-    _2 = false
-    i = 0
-    _2_at = 0
-    _3 = false
-    _3_at = 0
+  def full_house
+    two_pair = @tallies.find_index(2)
+    three_pair = @tallies.find_index(3)
 
-    tallies = [0]*6
-    tallies[d1-1] += 1
-    tallies[d2-1] += 1
-    tallies[d3-1] += 1
-    tallies[d4-1] += 1
-    tallies[d5-1] += 1
+    return two_pair * 2 + three_pair * 3 if two_pair && three_pair
 
-    for i in Array 0..5
-      if (tallies[i] == 2)
-        _2 = true
-        _2_at = i+1
-      end
-    end
-
-    for i in Array 0..5
-      if (tallies[i] == 3)
-        _3 = true
-        _3_at = i+1
-      end
-    end
-
-    if (_2 and _3)
-      return _2_at * 2 + _3_at * 3
-    else
-      return 0
-    end
+    0
   end
 end
